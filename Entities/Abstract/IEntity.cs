f442d3eabc24dc1af81b9470aa58efe4de6c1fc8
pc <pc@DESKTOP-VAGMEPN>
2021-02-06 00:03:59 +0300

    Add project files.

---
+++ Entities/Abstract/IEntity.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Entities.Abstract
+{
+    //IEntity imlement eden class bir veri tabanı tablosudur
+    public interface IEntity
+    {
+    }
+}
